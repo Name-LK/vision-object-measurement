ba28467d5c190fdd9c0815fd09758fdafaaa844d
lk <l250799k@outlook.com>
2025-10-30 21:25:48 -0300

    [feat] implementation of rgb_live_capture

--- src/echo_vision/logic/rgb_capture.py
+++ src/echo_vision/logic/rgb_capture.py
@@ -1,2 +1,14 @@
+from echo_vision.utils.config_loader import load_config
+from echo_vision.capture.factory import SensorFactory
+from echo_vision.views.plotter import display_live_rgb_capture
+
+#Needed steps -> load_config - get sensor type of the loaded config - create the Capturer - pass the capturer to the rgb plotter method
 def rgb_live_pipeline():
-    print("RGB live is running")
+    try:
+        config = load_config('config.yaml')
+        sensor_type = config.get("sensor", {}).get("type", "kinect_v1")
+        capturer = SensorFactory.create_capturer(sensor_type, config)
+        display_live_rgb_capture(capturer=capturer)
+        
+    except (ValueError, RuntimeError, FileNotFoundError) as e:
+         print(f"Critical Error: {e}")
